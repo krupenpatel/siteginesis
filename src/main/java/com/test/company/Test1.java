7bcbc39dd6ac2400aaa739aaac2700f38c903bd7
DhavalDevangada <DhavalDevangada@DESKTOP-172AK3E>
2021-03-01 21:01:37 +0530

    Added Test1 class

---
+++ src/main/java/com/test/company/Test1.java
@@ -0,0 +1,11 @@
+package com.test.company;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test	
+private void test() {
+	// TODO Auto-generated method stub
+System.out.println("this is simple test");
+}
+}
